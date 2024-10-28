4f6bdec6796d525b7c57e417ca6deee08f5b8b34
onehinny <8193447+onehinny@users.noreply.github.com>
2024-10-28 20:26:07 +0100

    adapt case options

--- hosts/backup/backup-ssh-command.nix
+++ hosts/backup/backup-ssh-command.nix
@@ -8,10 +8,13 @@
             echo "Shutting down host.";
             sudo shutdown now;
             ;;
-          *)
+        *borg*)
             cd /data;
             borg serve --lock-wait 600 --restrict-to-path /data;
             ;;
+        *)
+          echo "not allowed";
+          ;;
         esac
     '')
   ];

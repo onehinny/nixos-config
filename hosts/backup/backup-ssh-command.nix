79391a5769501cc8000c9a7e076441e0db769725
onehinny <8193447+onehinny@users.noreply.github.com>
2024-10-28 20:22:45 +0100

    adapt ssh command script

--- hosts/backup/backup-ssh-command.nix
+++ hosts/backup/backup-ssh-command.nix
@@ -1,17 +1,16 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
+ environment.systemPackages = with pkgs; [
     (writeShellScriptBin "ssh_command" ''
       case "$SSH_ORIGINAL_COMMAND" in 
         *shutdown*)    
-            echo "Shutting down host."
-            sudo shutdown now
+            echo "Shutting down host.";
+            sudo shutdown now;
             ;;
           *)
-            echo "serving borg ... \n"
-            cd /data
-            borg serve --lock-wait 600 --restrict-to-path /data
+            cd /data;
+            borg serve --lock-wait 600 --restrict-to-path /data;
             ;;
         esac
     '')
